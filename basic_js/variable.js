0de9ec149c5c155007273c45125403843e7612c1
Chandra Prakash <153980403+cpchoudhary143@users.noreply.github.com>
2024-10-12 13:48:02 +0000

    config done

--- basic_js/variable.js
+++ basic_js/variable.js
@@ -4,9 +4,11 @@ var accountPassword = "123456"
 userCity = "Jaipur"
 
 
+
 accountEmail = "cpchoudhary@cp.com"
 accountPassword = "cp@123456"
 userCity = "Kolkata"
+let userState;
 
-console.table([accountId, accountEmail, accountPassword, userCity])
+console.table([accountId, accountEmail, accountPassword, userCity, userState])
 console.log("aacountId");
