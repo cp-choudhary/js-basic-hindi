08bf888e79532cb1b9fc047f8494242625f71863
Chandra Prakash <153980403+cpchoudhary143@users.noreply.github.com>
2024-10-12 13:44:37 +0000

    config done

---
+++ basic_js/variable.js
@@ -0,0 +1,12 @@
+const accountId = "cp123"
+let accountEmail = "cp@gmail.com"
+var accountPassword = "123456"
+userCity = "Jaipur"
+
+
+accountEmail = "cpchoudhary@cp.com"
+accountPassword = "cp@123456"
+userCity = "Kolkata"
+
+console.table([accountId, accountEmail, accountPassword, userCity])
+console.log("aacountId");
