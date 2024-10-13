8ad376099ded5da7193ff7965b58decbc8c86b64
Chandra Prakash <153980403+cp-choudhary@users.noreply.github.com>
2024-10-13 13:01:05 +0000

    config done

---
+++ basic_js/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+ // conversionOperation 
+// "33" => 33
+// "22abs"=> NaN
+// true => 1; false => 0
+// 
