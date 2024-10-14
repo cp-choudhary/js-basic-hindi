6defaa5abe8ed317c2a44fec879d4886cf3cd75f
Chandra Prakash <153980403+cp-choudhary@users.noreply.github.com>
2024-10-14 15:06:25 +0000

    done

---
+++ basic_js/datatype.js
@@ -0,0 +1,26 @@
+// Primitive 
+
+// 7 types of primitive datatypes: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+const Score = 100
+const String = "CP Choudhary"
+const isLoggedIn = false
+
+const Number = null
+const bigNumber = 93749704812648362894n
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+console.log(typeof bigNumber);
+
+// Refrence(Non-Primitive):
+// Array, Objects, Functions
+
+const myFunction = function(){
+    console.log(hello world);
+    
+}
+
